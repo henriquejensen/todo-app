291116795ea2af85ac516db2ce5a7a786a0b69dc
Henrique Jensen <henriquejensen@hotmail.com>
2023-02-11 18:02:54 -0300

    Tela Login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import GetStarted from './pages/GetStarted';
 import styles from './App.module.css';
 import Onboard from './pages/Onboard';
+import Login from './pages/Login';
 
 function App() {
   return (
     <main className={ styles.container }>
       {/* <GetStarted /> */}
-      <Onboard />
+      {/* <Onboard /> */}
+      <Login />
     </main>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Banner from '../components/Banner';
+import Button from '../components/Button';
+import Form from '../components/Form';
+import Input from '../components/Input';
+import Subtitle from '../components/Subtitle';
+import Text from '../components/Text';
+import Title from '../components/Title';
+import styles from './Login.module.css';
+import LoginBanner from '../assets/LoginBanner.svg';
+
+function Login() {
+  return (
+    <section className={ styles.container }>
+      <Title title="Welcome Back!" />
+
+      <Banner image={ LoginBanner } />
+
+      <Form>
+        <Input placeholder="Enter your Email address" type="email" required />
+        <Input placeholder="Confirm Password" type="password" required />
+
+        <Subtitle subtitle="Forgot Password ?" />
+
+        <Button type="submit">Sign In</Button>
+      </Form>
+
+      <Text text="Dont have an account ? Sign Up" />
+    </section>
+  );
+}
+
+export default Login;
